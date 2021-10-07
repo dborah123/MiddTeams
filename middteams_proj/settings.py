c7b28305c1104082e21ccb00edbf246dc70938cd
dborah123 <dborah@middlebury.edu>
2021-10-06 20:41:35 -0400

    first real commit! 10/6/21

--- middteams_proj/settings.py
+++ middteams_proj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', '140.82.114.3']
 
 
 # Application definition
--- workouts/forms.py
+++ workouts/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class WorkoutCreationForm(forms.Form):
     # input fields you want user to input here...some of it can be handled by the view itself
-    # so don't worry about covering every field in the Workout model (ie...don't worry about 
+    # so don't worry about covering every field in the Workout model (ie don't worry about 
     # user...we can get that ourselves)
