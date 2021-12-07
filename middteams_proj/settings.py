b28c601910834ab33fadc7d56f00074cf3a38a6f
dborah123 <dborah@middlebury.edu>
2021-12-06 23:41:21 -0500

    configed debug value

--- middteams_proj/settings.py
+++ middteams_proj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = config("SECRET_KEY")
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = (config("SECRET_KEY") == "True")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if (config("DEBUG_VALUE")):
