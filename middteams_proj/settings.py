174daf5f4099b53a94eb0279d56942c8007a1bcc
dborah123 <dborah@middlebury.edu>
2021-12-06 23:26:56 -0500

    django_heroku.settings

--- middteams_proj/settings.py
+++ middteams_proj/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -154,3 +155,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
