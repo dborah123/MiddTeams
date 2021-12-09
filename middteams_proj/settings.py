ccf7e95294b3d6e5d754303531ad90eb75412347
dborah123 <dborah@middlebury.edu>
2021-12-09 08:33:32 -0500

    login message added

--- middteams_proj/settings.py
+++ middteams_proj/settings.py
@@ -23,14 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config("SECRET_KEY")
-SECRET_KEY = (config("SECRET_KEY") == "True")
+# SECRET_KEY = config("SECRET_KEY
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if (config("DEBUG_VALUE")):
-    DEBUG = True
-else:
-    DEBUG = False
+DEBUG = (config("DEBUG_VALUE") == "True")
 
 ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', '140.82.114.3', 'middteams.herokuapp.com']
 
